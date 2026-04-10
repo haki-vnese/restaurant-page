26aa2a7c7ea6afbc78eca5d199aa4b78ea3be1b7
Haky <dinhkhanghuy2061@gmail.com>
2026-04-10 01:32:23 -0700

    added EventListener and switching logic to main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("JavaScript is working!");
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
+
+function clearContent() {
+  const content = document.getElementById("content");
+  content.textContent = "";
+}
+
+function initializeWebsite() {
+  loadHome();
+
+  const homeBtn = document.getElementById("home-btn");
+  const menuBtn = document.getElementById("menu-btn");
+  const contactBtn = document.getElementById("contact-btn");
+
+  homeBtn.addEventListener("click", () => {
+    clearContent();
+    loadHome();
+  });
+
+  menuBtn.addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+  });
+
+  contactBtn.addEventListener("click", () => {
+    clearContent();
+    loadContact();
+  });
+}
+
+initializeWebsite();
